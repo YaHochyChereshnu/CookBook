10a93c85921188a1e17f1878c2569e6f4862807c
YaHochyChereshnu <75981571+YaHochyChereshnu@users.noreply.github.com>
2022-04-25 16:16:27 +0300

    Add files via upload

---
+++ CookBook-Pleshkov_Semenuk/CookBook-Pleshkov_Semenuk WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  CookBook-Pleshkov_Semenuk WatchKit Extension
+//
+//  Created by Student on 20.04.2022.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ CookBook-Pleshkov_Semenuk/CookBook-Pleshkov_Semenuk WatchKit Extension/RowTable.swift
@@ -0,0 +1,14 @@
+//
+//  RowTable.swift
+//  CookBook-Pleshkov_Semenuk WatchKit Extension
+//
+//  Created by Student on 20.04.2022.
+//
+
+import WatchKit
+
+class RowTable: NSObject {
+    @IBOutlet weak var rowRicture: WKInterfaceImage!
+    @IBOutlet weak var rowRecipeName: WKInterfaceLabel!
+
+}
